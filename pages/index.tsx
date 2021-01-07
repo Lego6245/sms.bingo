fe94c827c62ee167930f33e1688678b51143d53c
William S <lego6245@gmail.com>
2021-01-06 20:02:22 -0800

    minor polish

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,10 +19,13 @@ export default function Home() {
             <h1 className="text-5xl font-bold text-white">
             Super Mario Sunshine 1v1 Lockout Bingo League
             </h1>
+            <h2 className="text-4xl font-bold text-white my-5">
+            Starting January 13th
+            </h2>
           </header>
-          <div className="my-10 flex flex-row justify-center">
-            <a className="bg-yellow-300 hover:bg-yellow-500 rounded-md text-black font-bold m-2 p-2 text-2xl w-1/6" href="https://forms.gle/96oK5mUZrmpTuT9W6">Sign Up</a>
-            <a className="bg-yellow-300 hover:bg-yellow-500 rounded-md text-black font-bold m-2 p-2 text-2xl w-1/6" href="https://discord.gg/MVnV2qb">Join the Discord</a>
+          <div className="flex flex-row justify-center">
+            <a className="bg-yellow-300 hover:bg-yellow-500 rounded-md text-black font-bold m-2 p-2 text-2xl w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6" href="https://forms.gle/96oK5mUZrmpTuT9W6">Sign Up</a>
+            <a className="bg-yellow-300 hover:bg-yellow-500 rounded-md text-black font-bold m-2 p-2 text-2xl w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6" href="https://discord.gg/MVnV2qb">Join the Discord</a>
           </div>
         </main>
       </div>
