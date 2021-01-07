92f87db9f88d57b568269235164eb6f7bff7c763
William S <lego6245@gmail.com>
2021-01-06 19:24:46 -0800

    discord

---
+++ pages/discord.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <meta http-equiv="refresh" content="0;url=https://discord.gg/MVnV2qb" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+          If you are not redirected, click <a href="https://discord.gg/MVnV2qb">here.</a>
+      </div>
+    </div>
+  )
+}
