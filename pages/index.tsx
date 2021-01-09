38d3bbd62dbfa1dedf276cab1edde19255de78a9
William S <lego6245@gmail.com>
2021-01-09 00:31:22 -0800

    cute division

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,15 +52,19 @@ export default function Home(props: HomeProps) {
             </div>
             <div className="fixed min-h-screen min-w-full">
                 <main className={'min-h-screen flex flex-col justify-center text-center z-20'}>
-                    <header>
-                        <h1 className="text-5xl font-bold text-white">
+                    <div>
+                        <div className="h-auto text-5xl font-bold text-white">
                             Super Mario Sunshine 1v1 Lockout Bingo League
-                        </h1>
-                        <h2 className="text-4xl font-bold text-white my-5 subheadAnimation">
-                            <span className="text-yellow-300">{startHere[startHereIndex][0]}</span>
-                            <span className="text-blue-400">{startHere[startHereIndex][1]}</span>
-                        </h2>
-                    </header>
+                        </div>
+                        <div className="flex flex-row justify-center text-4xl font-bold text-white my-5 subheadAnimation">
+                            <div className="text-yellow-300 text-right w-1/2 mr-2">
+                                {startHere[startHereIndex][0]}
+                            </div>
+                            <div className="text-blue-400 text-left w-1/2 ml-2">
+                                {startHere[startHereIndex][1]}
+                            </div>
+                        </div>
+                    </div>
                     <div className="flex flex-row justify-center">
                         <Link href="/standings">
                             <div className="cursor-pointer transition-colors bg-yellow-300 hover:bg-yellow-500 rounded-md text-black font-bold m-2 p-2 text-2xl w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6">
