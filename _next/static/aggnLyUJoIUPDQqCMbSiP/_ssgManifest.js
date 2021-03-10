4ef60dd2fb2112e308889a1e1cf19ccce51ca547
John Bingo <knowyourwinconditions@github.com>
2021-03-10 16:44:25 +0000

    deploy: 2e325f8d3b0f81509ed0e7f17df024bc4b29b1f9

---
+++ _next/static/aggnLyUJoIUPDQqCMbSiP/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fstandings","\u002F","\u002Fschedule","\u002Fupcoming","\u002Fplayer\u002F[playername]","\u002Fupcoming\u002F[calendarweek]\u002F[day]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
