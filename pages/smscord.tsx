e4c4263354238adac0848c70aaad066b8b83b71c
Lego <lego6245@gmail.com>
2021-10-29 10:08:34 -0700

    add smscord shortlink

---
+++ pages/smscord.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Discord() {
+    return (
+        <div>
+            <Head>
+                <meta http-equiv="refresh" content="0;url=https://discord.gg/jX38aXCv9y" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div>
+                If you are not redirected, click <a href="https://discord.gg/jX38aXCv9y">here.</a>
+            </div>
+        </div>
+    );
+}
